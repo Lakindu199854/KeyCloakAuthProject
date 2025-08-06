3cb5808c75a7521ddfc8a792df7e375568e2a027
Lakindu199854 <lakinduat@gmail.com>
2025-08-06 15:21:54 +0400

    Angular app changes

--- oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/controller/HomeController.java
+++ oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/controller/HomeController.java
@@ -1,4 +1,16 @@
 package com.programming.techie.authcode.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        //"home" is the name of the html file that is going to serve the request
+        //When a request comes to the /home then request will be redirected to the home.html page
+        return "home";
+    }
 }
+
