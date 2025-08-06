4dd4e746d8b79b97d90ede574a2e9f53865abfff
Lakindu199854 <lakinduat@gmail.com>
2025-08-06 23:18:52 +0400

    Added security configs

--- oauth2-pkce-demo/src/main/java/com/programming/techie/pkce/config/SecurityConfig.java
+++ oauth2-pkce-demo/src/main/java/com/programming/techie/pkce/config/SecurityConfig.java
@@ -1,4 +1,21 @@
 package com.programming.techie.pkce.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/home/**"))
+                .authorizeHttpRequests(httpRequests -> httpRequests.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2resourceServer->oauth2resourceServer.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
